4bf546eab4c57f37fdf206583d81ad9e20bec2d3
Vladyslav Pleshko <vldsv.pl1@gmail.com>
2025-10-01 18:17:54 +0300

    Updated columns for Pydantic

--- app/models.py
+++ app/models.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel
 
 class InputData(BaseModel):
     category: str
-    level: str
-    english: str
-    experience_years: int
+    title_group: str
+    english_level: str
+    it_experience_years: int
 
 class OutputData(BaseModel):
     prediction: int
