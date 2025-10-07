9a734e4fcc1cc4f78dd429e33dfe384f56770d6e
Vladyslav Pleshko <vldsv.pl1@gmail.com>
2025-10-07 17:19:18 +0300

    refactor: update code (from notebooks)

--- src/data/pipeline.py
+++ src/data/pipeline.py
@@ -5,6 +5,8 @@ from src.utils.paths import DATA_DIR
 from src.data.preprocessing import select_and_rename_features
 
 
-def run_pipeline(input_csv, output_csv):
+def run_pipeline(input_csv, output_csv=None):
     df = pd.read_csv(input_csv, encoding='cp1251')
     df = select_and_rename_features(df)
+
+    return df
--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+from src.data.configs.seniority_mapping import SENIORITY_LEVEL_MAPPING
+
 def select_and_rename_features(df: pd.DataFrame) -> pd.DataFrame:
     """
     Обирає ключові колонки з сирого датасету,
@@ -9,14 +11,19 @@ def select_and_rename_features(df: pd.DataFrame) -> pd.DataFrame:
 
     selected_features = {
     'ЗАРПЛАТА / СУМАРНИЙ ДОХІД в IT у $$$ за місяць, лише ставка \nЧИСТИМИ - після сплати податків': 'salary_usd',
-    'Тайтл': 'position_level',
-    'Категорія': 'category',
+    'Тайтл': 'seniority_level',
     'Почніть вводити і оберіть вашу ОСНОВНУ посаду зі списку': 'position',
     'Знання англійської мови': 'english_level',
-    'Загальний стаж роботи за нинішньою ІТ-спеціальністю': 'it_experience_years',
+    'Загальний стаж роботи за нинішньою ІТ-спеціальністю': 'experience_years',
 }
     df_selected = df[list(selected_features.keys())].copy()
     df_selected.rename(columns=selected_features, inplace=True)
     df_selected = df_selected.dropna(subset=['salary_usd'])
 
     return df_selected
+
+def standardization_seniority_features(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Уніфікація значень для рівнів експертності
+    """
+    df['seniority_level'] = df['seniority_level'].map(SENIORITY_LEVEL_MAPPING)
