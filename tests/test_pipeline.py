1abf127d3ca031ef4826c5e81597cd1a9e4ac7fb
Vladyslav Pleshko <vldsv.pl1@gmail.com>
2025-10-07 22:45:55 +0300

    chore: update module for testing pipeline

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -4,7 +4,9 @@ from src.utils.paths import DATA_DIR
 from src.data.pipeline import run_pipeline
 
 def test_pipeline_flow():
-    result = run_pipeline(input_csv= DATA_DIR / 'raw/2025_june_raw.csv')
-    result.info()
+    df = run_pipeline(input_csv= DATA_DIR / 'raw/2025_june_raw.csv',
+                      save=False)
 
-test_pipeline_flow()
+    assert isinstance(df, pd.DataFrame)
+    assert not df.isna().values.any(), "DataFrame містить NaN"
+    assert (DATA_DIR / "processed/model_input_df.csv").exists()
